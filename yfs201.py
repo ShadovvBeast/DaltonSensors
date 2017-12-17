2be6dc71b774d2c3a9ab8cc433f84f6ec8bbc78d
Graphene <graphenesoft@gmail.com>
2017-12-17 19:55:10 +0200

    testing stuff

--- yfs201.py
+++ yfs201.py
@@ -1,13 +1,13 @@
 import RPi.GPIO as GPIO
 
 # to use Raspberry Pi board pin numbers
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 # set up the GPIO channels - one input and one output
 GPIO.setup(11, GPIO.IN)
 
 # input from pin 11
-input_value = GPIO.input(11)
+input_value = GPIO.input(5)
 
 print(input_value)
 # output to pin 12
