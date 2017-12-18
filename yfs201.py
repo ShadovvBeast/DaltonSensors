a2ab0fe4930ea30548cf48faae1b6f44de272e71
Graphene <graphenesoft@gmail.com>
2017-12-18 18:08:19 +0200

    https://www.youtube.com/watch?v=8JXd-2_zJQ0 code

--- yfs201.py
+++ yfs201.py
@@ -1,14 +1,43 @@
 import RPi.GPIO as GPIO
-
-# to use Raspberry Pi board pin numbers
+import time, sys
+pin_number = 5
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_number, GPIO.IN)
 
-# set up the GPIO channels - one input and one output
-GPIO.setup(5, GPIO.IN)
+rate_cnt = 0 #Rate of counts (L/min
+tot_cnt = 0 #Total counts (total litres)
+minutes = 0 #Total minutes
+constant = 0.10#Convert pulses to litres
+time_new = 0.0#Keep next time
+#input_value = GPIO.input(pin_number)
 
-# input from pin 11
-input_value = GPIO.input(5)
+print('Water Flow - Approximate')
+print('Control C to exit')
 
-print(input_value)
+while True:
+    time_new = time.time() + 10 # + 60 = 1 minute
+    rate_cnt = 0
+    while time.time() <= time_new:
+        input_value = GPIO.input(pin_number) # Look for pulses
+        if input_value != 0:
+            rate_cnt += 1   # Pulses per time
+            tot_cnt += 1 # Total pulses
+            try:
+                input_value = GPIO.input(pin_number)
+                print(input_value, end = '')
+            except  KeyboardInterrupt: #Look for exit command
+                print('\nCTRL C - Exiting nicely')
+                GPIO.cleanup()
+                sys.exit()
+    minutes += 1
+    print ('\nLitres / min', round(rate_cnt * constant, 4))
+    print('\nTotal Litres', round(tot_cnt * constant, 4))
+    print('\nTime (min & clock)', minutes, '\t', time.asctime(time_new))
+GPIO.cleanup()
+print('Done');
+
+
+
+#print(input_value)
 # output to pin 12
 #GPIO.output(12, GPIO.HIGH)
