c2cd09f0fe54513f280d09685d3b871adfe1b7de
Graphene <graphenesoft@gmail.com>
2018-03-05 12:29:25 +0200

    temperature fixes

--- temperature.py
+++ temperature.py
@@ -2,6 +2,5 @@ from ds18b20 import DS18B20
 # test temperature sensors
 x = DS18B20()
 count=x.device_count()
-i = 0
-while i < count:
-        print(x.tempC(i))
+for i in range(0, count):
+    print(x.tempC(i))
