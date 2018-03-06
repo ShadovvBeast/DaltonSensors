41d91fd3620d1461c1d0d9e3705bf7dc39041ce0
Graphene <graphenesoft@gmail.com>
2018-03-06 12:12:15 +0200

    send request

--- temperature.py
+++ temperature.py
@@ -4,4 +4,6 @@ import requests
 x = DS18B20()
 count=x.device_count()
 for i in range(0, count):
-    print(x._device_file[i][-21:][:12] + ' - ' + str(x.tempC(i)));
+    r = requests.post("http://www.dalton.farm/sensors/insert_data.php", data={'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': 'temperature ' + str(i), 'sensor_type': 'temperature', 'value': x.tempC(i)})
+    print(r.status_code, r.reason)
+    print(r.text[:300] + '...')
