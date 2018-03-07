12dfda79c148b02795ca233e463e6e0c98d6c165
Graphene <graphenesoft@gmail.com>
2018-03-07 22:39:16 +0200

    pi serial

--- temperature.py
+++ temperature.py
@@ -1,6 +1,8 @@
 from ds18b20 import DS18B20
 import requests
 import pymongo
+import util
+
 client = pymongo.MongoClient('mongodb://dalton:!df420DF!@ds261088.mlab.com:61088/dalton')
 db = client.dalton
 collection = db.dt_sensor_data
@@ -8,7 +10,7 @@ collection = db.dt_sensor_data
 x = DS18B20()
 count=x.device_count()
 for i in range(0, count):
-    d = {'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': 'temperature ' + str(i), 'sensor_type': 'temperature', 'value': x.tempC(i)}
+    d = {'pi_serial': util.getserial(), 'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': 'temperature ' + str(i), 'sensor_type': 'temperature', 'value': x.tempC(i)}
     r = requests.post("http://www.dalton.farm/sensors/insert_data.php", data=d)
     id = collection.insert_one(d).inserted_id
     print('Mongo id:', id)
