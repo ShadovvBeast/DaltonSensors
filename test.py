49ba707fc41575653bef56bf56127a6d2f931abe
Graphene <graphenesoft@gmail.com>
2018-03-07 15:15:45 +0200

    test mongodb

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pymongo
-client = pymongo.MongoClient('mongodb://dalton:!df420DF!@ds261088.mlab.com:61088/dalton')
+client = pymongo.MongoClient('mongodb://ShadovvBeast:!df420DF!@ds261088.mlab.com:61088/dalton')
 db = client.dalton
 collection = db.dt_dalton_sensors
 id = collection.insert_one({'test':'testval'}).inserted_id
