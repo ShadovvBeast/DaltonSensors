12f2127ac7244588b3ec8650ba6bd740463ca96e
Graphene <graphenesoft@gmail.com>
2018-03-07 15:12:30 +0200

    get raw temp value

--- temperature.py
+++ temperature.py
@@ -1,9 +1,12 @@
 from ds18b20 import DS18B20
 import requests
+import pymongo
+client = pymongo.MongoClient('mongodb://dalton:!df420DF!@ds261088.mlab.com:61088/dalton')
 # test temperature sensors
 x = DS18B20()
 count=x.device_count()
 for i in range(0, count):
-    r = requests.post("http://www.dalton.farm/sensors/insert_data.php", data={'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': 'temperature ' + str(i), 'sensor_type': 'temperature', 'value': x.tempC(i)})
+    d = {'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': 'temperature ' + str(i), 'sensor_type': 'temperature', 'value': x.tempC(i)}
+    r = requests.post("http://www.dalton.farm/sensors/insert_data.php", data=d)
     print(r.status_code, r.reason)
     print(r.text[:300] + '...')
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 import pymongo
-client = pymongo.MongoClient('mongodb://dalton:!df420DF!@ds261088.mlab.com:61088/dalton');
+client = pymongo.MongoClient('mongodb://dalton:!df420DF!@ds261088.mlab.com:61088/dalton')
+db = client.dalton
+collection = db.dt_dalton_sensors
+id = collection.insert_one({'test':'testval'}).inserted_id
+print(id)
