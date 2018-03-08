213ba2f5a2714cadbf8fca18234a4516833deabc
Graphene <graphenesoft@gmail.com>
2018-03-08 14:44:00 +0200

    yfs201 database

--- yfs201.py
+++ yfs201.py
@@ -1,11 +1,14 @@
 import RPi.GPIO as GPIO
 import time, sys
+import database
+import util
+
 pin_number = 13
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin_number, GPIO.IN)
 
 rate_cnt = 0 #Rate of counts (L/min
-tot_cnt = 0 #Total counts (total litres)
+#tot_cnt = 0 #Total counts (total litres)
 minutes = 0 #Total minutes
 constant = 0.10#Convert pulses to litres
 time_new = 0.0#Keep next time
@@ -19,23 +22,26 @@ while time.time() <= time_new:
     input_value = GPIO.input(pin_number) # Look for pulses
     if input_value != 0:
         rate_cnt += 1   # Pulses per time
-        tot_cnt += 1 # Total pulses
+ #       tot_cnt += 1 # Total pulses
         try:
             input_value = GPIO.input(pin_number)
-            sys.stdout.write(str(input_value)),
+            #sys.stdout.write(str(input_value)),
         except  KeyboardInterrupt: #Look for exit command
             print('\nCTRL C - Exiting nicely')
             GPIO.cleanup()
             sys.exit()
 
 minutes += 1
-print ('\nLitres / min', round(rate_cnt * constant, 4))
-print('\nTotal Litres', round(tot_cnt * constant, 4))
+value = rate_cnt * constant
+print ('\nLitres / min', round(value, 4))
+#print('\nTotal Litres', round(tot_cnt * constant, 4))
 print('\nTime (min & clock)', minutes)
  #   raw_input("Press the <ENTER> key to continue...")
 
 GPIO.cleanup()
-print('Done');
+
+if (database.insert_data({'sensor_id': 'WATERFLOW_' + util.getserial(), 'sensor_name': 'water flow 0', 'sensor_type': 'YFS201', 'value': value})):
+    print('Water flow inserted successfully')
 
 
 
