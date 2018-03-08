ccb90b760ce1a2faaf2d504ed96c96b4f04500da
Graphene <graphenesoft@gmail.com>
2018-03-08 11:53:44 +0200

    mongo timestamp

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
 import pymongo
 import requests
+import datetime
 client = pymongo.MongoClient('mongodb://dalton:!df420DF!@ds261088.mlab.com:61088/dalton')
 db = client.dalton
 collection = db.dt_sensor_data
 
 def insert_data(data):
     r = requests.post("http://www.dalton.farm/sensors/insert_data.php", data=data)
+    data.timestamp = str(datetime.datetime.utcnow())
     id = collection.insert_one(data).inserted_id
     return (id and r.status_code == 200)
