0c5b5d50671fbcd309d115f961596a3675b36504
Graphene <graphenesoft@gmail.com>
2018-03-08 12:23:57 +0200

    mcp values to db

--- mcp3008.py
+++ mcp3008.py
@@ -3,6 +3,8 @@ import time
 # Import SPI library (for hardware SPI) and MCP3008 library.
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
+import database
+import util
 
 # Hardware SPI configuration:
 SPI_PORT   = 0
@@ -10,8 +12,15 @@ SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 mcp_test = mcp.read_adc(0)
-ambiance = mcp.read_adc(1)
+ambience = mcp.read_adc(1)
 ph = mcp.read_adc(2)
 humidity = mcp.read_adc(3)
 
-print(mcp_test, ambiance, ph, humidity)
+if (database.insert_data({'sensor_id': 'AMBIENCE_' + util.getserial(), 'sensor_name': 'ambience 0', 'sensor_type': 'TEMT6000', 'value': ambience})):
+    print('Ambiance inserted successfully')
+if(database.insert_data({'sensor_id': 'PH_' + util.getserial(), 'sensor_name': 'ph 0', 'sensor_type': 'ph', 'value': ph})):
+    print('pH inserted successfully')
+if (database.insert_data({'sensor_id': 'HUMIDITY_' + util.getserial(), 'sensor_name': 'humidity 0', 'sensor_type': 'HR202L', 'value': humidity})):
+    print('Humidity inserted successfully')
+
+
