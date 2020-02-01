ea90e56ccf66cd8fbf68aed65c74a287c3f32fdd
ShadowBeast <ShadovvBeast@gmail.com>
2020-02-01 13:55:55 +0200

    elasticsearch 7 compatability

--- temperature.py
+++ temperature.py
@@ -6,8 +6,8 @@ x = DS18B20()
 count=x.device_count()
 for i in range(0, count):
     name = 'temperature ' + str(i)
-    d = {'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': name, 'value': x.tempC(i)}
-    res = connection.indices.create(index="sensors", doc_type='temperature', body=d)
-    if(res):
+    d = {'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': name, 'sensor_type': 'temperature', 'value': x.tempC(i)}
+    res = connection.indices.create(index="sensors", body=d)
+    if res:
         print('Temperature inserted successfully')
         print res
