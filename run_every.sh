91a24b25bd51fab85d261eec71dc06ed94b6b212
ShadowBeast <ShadovvBeast@gmail.com>
2020-02-01 14:45:35 +0200

    run every -> only temperature for now

--- run_every.sh
+++ run_every.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 while true
-cnt=0
+#cnt=0
 do
-    if [ $(( $cnt%4 )) -eq 0 ]; then
-        nohup python yfs201.py &>/dev/null &
-    fi
+    #if [ $(( $cnt%4 )) -eq 0 ]; then
+    #    nohup python yfs201.py &>/dev/null &
+    #fi
     python temperature.py
-    python mcp3008.py
+    #python mcp3008.py
     sleep 5
-    ((cnt+=5))
+    #((cnt+=5))
 done
