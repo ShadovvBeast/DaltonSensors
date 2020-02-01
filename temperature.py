13325f18cae634cc49aefb0941479a77644b40fa
ShadowBeast <ShadovvBeast@gmail.com>
2020-02-01 13:45:38 +0200

    remove refresh

--- temperature.py
+++ temperature.py
@@ -7,7 +7,7 @@ count=x.device_count()
 for i in range(0, count):
     name = 'temperature ' + str(i)
     d = {'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': name, 'value': x.tempC(i)}
-    res = connection.indices.create(index="sensors", doc_type='temperature', body=d, refresh=True)
+    res = connection.indices.create(index="sensors", doc_type='temperature', body=d)
     if(res):
         print('Temperature inserted successfully')
         print res
