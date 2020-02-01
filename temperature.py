3f43e4e68d52c35883a46fa72ce0dcac7477ff05
ShadowBeast <ShadovvBeast@gmail.com>
2020-02-01 13:42:34 +0200

    add elasticsearch to temperature

--- temperature.py
+++ temperature.py
@@ -1,10 +1,13 @@
 from ds18b20 import DS18B20
-import database
-
+from elasticsearch import Elasticsearch
+connection = Elasticsearch(["http://192.168.1.11:9200"])
 # test temperature sensors
 x = DS18B20()
 count=x.device_count()
 for i in range(0, count):
-    d = {'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': 'temperature ' + str(i), 'sensor_type': 'temperature', 'value': x.tempC(i)}
-    if(database.insert_data(d)):
+    name = 'temperature ' + str(i)
+    d = {'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': name, 'value': x.tempC(i)}
+    res = connection.indices.create(index="sensors", doc_type='temperature', body=d, refresh=True)
+    if(res):
         print('Temperature inserted successfully')
+        print res
