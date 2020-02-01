0588403b45dad86996ede20a247dac2d604d30e1
Asaf <ShadovvBeast@gmail.com>
2020-02-01 12:21:07 +0000

    add timestamp to temperature

--- temperature.py
+++ temperature.py
@@ -1,13 +1,15 @@
 from ds18b20 import DS18B20
 from elasticsearch import Elasticsearch
+from datetime import datetime
+
 connection = Elasticsearch(["http://192.168.1.11:9200"])
 # test temperature sensors
 x = DS18B20()
 count=x.device_count()
 for i in range(0, count):
     name = 'temperature ' + str(i)
-    d = {'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': name, 'sensor_type': 'temperature', 'value': x.tempC(i)}
-    res = connection.indices.create(index="sensors", body=d)
+    d = {'sensor_id': x._device_file[i][-21:][:12], 'sensor_name': name, 'sensor_type': 'temperature', 'value': x.tempC(i), 'timestamp': datetime.now()}
+    res = connection.index(index="sensors", body=d)
     if res:
         print('Temperature inserted successfully')
         print res
